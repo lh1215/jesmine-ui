2473b6274406d755ef7276205b4ccde724b79005
liangh <1187283513@qq.com>
2020-11-24 14:21:47 +0800

    添加storybook插件

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/addon-viewport",
+    "@storybook/addon-knobs",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-storysource",
+  ],
+};
